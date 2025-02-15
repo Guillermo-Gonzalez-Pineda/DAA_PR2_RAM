7c3084677dbc4b777f51f677ca1d67328c02e5a2
Guillermo-Gonzalez-Pineda <alu0101574899@ull.edu.es>
2025-02-15 20:10:46 +0000

    Programa funcional. Falta implementacion *2 y mejorar main

---
+++ debug/CMakeFiles/RAM.dir/DependInfo.cmake
@@ -0,0 +1,28 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/guillermo/DAA/PR2_RAM/DAA_PR2_RAM/src/alu.cc" "/home/guillermo/DAA/PR2_RAM/DAA_PR2_RAM/debug/CMakeFiles/RAM.dir/src/alu.cc.o"
+  "/home/guillermo/DAA/PR2_RAM/DAA_PR2_RAM/src/elegir-instruccion.cc" "/home/guillermo/DAA/PR2_RAM/DAA_PR2_RAM/debug/CMakeFiles/RAM.dir/src/elegir-instruccion.cc.o"
+  "/home/guillermo/DAA/PR2_RAM/DAA_PR2_RAM/src/main.cc" "/home/guillermo/DAA/PR2_RAM/DAA_PR2_RAM/debug/CMakeFiles/RAM.dir/src/main.cc.o"
+  "/home/guillermo/DAA/PR2_RAM/DAA_PR2_RAM/src/memoria-datos.cc" "/home/guillermo/DAA/PR2_RAM/DAA_PR2_RAM/debug/CMakeFiles/RAM.dir/src/memoria-datos.cc.o"
+  "/home/guillermo/DAA/PR2_RAM/DAA_PR2_RAM/src/memoria-programa.cc" "/home/guillermo/DAA/PR2_RAM/DAA_PR2_RAM/debug/CMakeFiles/RAM.dir/src/memoria-programa.cc.o"
+  "/home/guillermo/DAA/PR2_RAM/DAA_PR2_RAM/src/unidad-alu.cc" "/home/guillermo/DAA/PR2_RAM/DAA_PR2_RAM/debug/CMakeFiles/RAM.dir/src/unidad-alu.cc.o"
+  "/home/guillermo/DAA/PR2_RAM/DAA_PR2_RAM/src/unidad-entrada.cc" "/home/guillermo/DAA/PR2_RAM/DAA_PR2_RAM/debug/CMakeFiles/RAM.dir/src/unidad-entrada.cc.o"
+  "/home/guillermo/DAA/PR2_RAM/DAA_PR2_RAM/src/unidad-salida.cc" "/home/guillermo/DAA/PR2_RAM/DAA_PR2_RAM/debug/CMakeFiles/RAM.dir/src/unidad-salida.cc.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../src"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ debug/CMakeFiles/RAM.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/RAM.dir/src/alu.cc.o"
+  "CMakeFiles/RAM.dir/src/elegir-instruccion.cc.o"
+  "CMakeFiles/RAM.dir/src/main.cc.o"
+  "CMakeFiles/RAM.dir/src/memoria-datos.cc.o"
+  "CMakeFiles/RAM.dir/src/memoria-programa.cc.o"
+  "CMakeFiles/RAM.dir/src/unidad-alu.cc.o"
+  "CMakeFiles/RAM.dir/src/unidad-entrada.cc.o"
+  "CMakeFiles/RAM.dir/src/unidad-salida.cc.o"
+  "RAM"
+  "RAM.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/RAM.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
